93d57416b31accdf99b389293bdcb971bb65922f
Pritam Kadam <kpritam@thoughtworks.com>
2021-05-30 11:39:35 +0530

    Simplify kitty

--- flake.nix
+++ flake.nix
@@ -16,119 +16,115 @@
 
     # Other sources
     # comma = { url = "github:Shopify/comma"; flake = false; };
-    flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-
   outputs = { self, nixpkgs, darwin, home-manager, flake-utils, ... }@inputs:
-  let
-    # Some building blocks --------------------------------------------------------------------- {{{
+    let
+      # Some building blocks --------------------------------------------------------------------- {{{
 
-    # Configuration for `nixpkgs` mostly used in personal configs.
-    nixpkgsConfig = with inputs; {
-      config = { allowUnfree = true; };
-      overlays = self.overlays ++ [
-        (
-          final: prev:
-          let
-            system = prev.stdenv.system;
-            nixpkgs-stable = if system == "x86_64-darwin" then nixpkgs-stable-darwin else nixos-stable;
-          in {
-            master = nixpkgs-master.legacyPackages.${system};
-            stable = nixpkgs-stable.legacyPackages.${system};
-          }
-        )
-      ];
-    };
-
-    # Personal configuration shared between `nix-darwin` and plain `home-manager` configs.
-    homeManagerCommonConfig = with self.homeManagerModules; {
-      imports = [
-        ./home
-        configs.starship.symbols
-        programs.kitty.extras
-      ];
-    };
-
-    # Modules shared by most `nix-darwin` personal configurations.
-    nixDarwinCommonModules = { user }: [
-      # Include extra `nix-darwin`
-      self.darwinModules.programs.nix-index
-      self.darwinModules.security.pam
-      # Main `nix-darwin` config
-      ./darwin
-      # `home-manager` module
-      home-manager.darwinModules.home-manager
-      {
-        nixpkgs = nixpkgsConfig;
-        # Hack to support legacy worklows that use `<nixpkgs>` etc.
-        nix.nixPath = { nixpkgs = "$HOME/.config/nixpkgs/nixpkgs.nix"; };
-        # `home-manager` config
-        users.users.${user}.home = "/Users/${user}";
-        home-manager.useGlobalPkgs = true;
-        home-manager.users.${user} = homeManagerCommonConfig;
-      }
-    ];
-    # }}}
-  in {
-
-    # Personal configuration ------------------------------------------------------------------- {{{
-
-    # My `nix-darwin` configs
-    darwinConfigurations = {
-      # Mininal configuration to bootstrap systems
-      bootstrap = darwin.lib.darwinSystem {
-        modules = [ ./darwin/bootstrap.nix { nixpkgs = nixpkgsConfig; } ];
-      };
-
-      # My macOS main laptop config
-      MacBookPro = darwin.lib.darwinSystem {
-        modules = nixDarwinCommonModules { user = "pritamkadam"; } ++ [
-          {
-            networking.computerName = "pritamkadam";
-            networking.hostName = "MacBook-Pro";
-            networking.knownNetworkServices = [
-              "Wi-Fi"
-              "USB 10/100/1000 LAN"
-            ];
-          }
+      # Configuration for `nixpkgs` mostly used in personal configs.
+      nixpkgsConfig = with inputs; {
+        config = { allowUnfree = true; };
+        overlays = self.overlays ++ [
+          (final: prev:
+            let
+              system = prev.stdenv.system;
+              nixpkgs-stable = if system == "x86_64-darwin" then
+                nixpkgs-stable-darwin
+              else
+                nixos-stable;
+            in {
+              master = nixpkgs-master.legacyPackages.${system};
+              stable = nixpkgs-stable.legacyPackages.${system};
+            })
         ];
       };
 
-    };
+      # Personal configuration shared between `nix-darwin` and plain `home-manager` configs.
+      homeManagerCommonConfig = with self.homeManagerModules; {
+        imports = [ ./home configs.starship.symbols ];
+      };
 
-    # }}}
-
-    # Outputs useful to others ----------------------------------------------------------------- {{{
-
-    overlays = with inputs; [
-      (
-        final: prev: {
-          # Some packages
-          # comma = import comma { inherit (prev) pkgs; };
+      # Modules shared by most `nix-darwin` personal configurations.
+      nixDarwinCommonModules = { user }: [
+        # Include extra `nix-darwin`
+        self.darwinModules.programs.nix-index
+        self.darwinModules.security.pam
+        # Main `nix-darwin` config
+        ./darwin
+        # `home-manager` module
+        home-manager.darwinModules.home-manager
+        {
+          nixpkgs = nixpkgsConfig;
+          # Hack to support legacy worklows that use `<nixpkgs>` etc.
+          nix.nixPath = { nixpkgs = "$HOME/.config/nixpkgs/nixpkgs.nix"; };
+          # `home-manager` config
+          users.users.${user}.home = "/Users/${user}";
+          home-manager.useGlobalPkgs = true;
+          home-manager.users.${user} = homeManagerCommonConfig;
         }
-      )
-      # Other overlays that don't depend on flake inputs.
-    ] ++ map import ((import ./lsnix.nix) ./overlays);
+      ];
+      # }}}
+    in {
 
-    # My `nix-darwin` modules that are pending upstream, or patched versions waiting on upstream
-    # fixes.
-    darwinModules = {
-      programs.nix-index = import ./darwin/modules/programs/nix-index.nix;
-      security.pam = import ./darwin/modules/security/pam.nix;
-    };
+      # Personal configuration ------------------------------------------------------------------- {{{
 
-    homeManagerModules = {
-      configs.starship.symbols = import ./home/configs/starship-symbols.nix;
-      programs.kitty.extras = import ./home/modules/programs/kitty/extras.nix;
-    };
-    # }}}
+      # My `nix-darwin` configs
+      darwinConfigurations = {
+        # Mininal configuration to bootstrap systems
+        bootstrap = darwin.lib.darwinSystem {
+          modules = [ ./darwin/bootstrap.nix { nixpkgs = nixpkgsConfig; } ];
+        };
 
-    # Add re-export `nixpkgs` packages with overlays.
-    # This is handy in combination with `nix registry add my /Users/malo/.config/nixpkgs`
-  } // flake-utils.lib.eachDefaultSystem (system: {
-      legacyPackages = import nixpkgs { inherit system; inherit (nixpkgsConfig) config overlays; };
-  });
+        # My macOS main laptop config
+        MacBookPro = darwin.lib.darwinSystem {
+          modules = nixDarwinCommonModules { user = "pritamkadam"; } ++ [{
+            networking.computerName = "pritamkadam";
+            networking.hostName = "MacBook-Pro";
+            networking.knownNetworkServices = [ "Wi-Fi" "USB 10/100/1000 LAN" ];
+          }];
+        };
+
+      };
+
+      # }}}
+
+      # Outputs useful to others ----------------------------------------------------------------- {{{
+
+      overlays = with inputs;
+        [
+          (final: prev:
+            {
+              # Some packages
+              # comma = import comma { inherit (prev) pkgs; };
+            })
+          # Other overlays that don't depend on flake inputs.
+        ] ++ map import ((import ./lsnix.nix) ./overlays);
+
+      # My `nix-darwin` modules that are pending upstream, or patched versions waiting on upstream
+      # fixes.
+      darwinModules = {
+        programs.nix-index = import ./darwin/modules/programs/nix-index.nix;
+        security.pam = import ./darwin/modules/security/pam.nix;
+      };
+
+      homeManagerModules = {
+        configs.starship.symbols = import ./home/configs/starship-symbols.nix;
+      };
+      # }}}
+
+      # Add re-export `nixpkgs` packages with overlays.
+      # This is handy in combination with `nix registry add my /Users/malo/.config/nixpkgs`
+    } // flake-utils.lib.eachDefaultSystem (system: {
+      legacyPackages = import nixpkgs {
+        inherit system;
+        inherit (nixpkgsConfig) config overlays;
+      };
+    });
 }
 # vim: foldmethod=marker
--- home/kitty.nix
+++ home/kitty.nix
@@ -1,26 +1,4 @@
-{ config, lib, pkgs, ... }:
-# Let-In --------------------------------------------------------------------------------------- {{{
-let
-  inherit (lib) mkIf;
-  backgroundDependantColors = colors: with colors; {
-    background = "#${base}";
-    foreground = "#${main}";
-
-    # Cursor
-    cursor = "#${blue}";
-    cursor_text_color = "#${base}";
-
-    # Selection
-    selection_background = "#${muted}";
-    selection_foreground = "#${base}";
-
-    # Tab bar
-    tab_bar_background = "#${basehl}";
-    inactive_tab_background = "#${strong}";
-  };
-in
-  # }}}
-{
+{ ... }: {
   # Kitty terminal
   # https://sw.kovidgoyal.net/kitty/conf.html
   # https://rycee.gitlab.io/home-manager/options.html#opt-programs.kitty.enable
@@ -29,73 +7,74 @@ in
   # General config ----------------------------------------------------------------------------- {{{
 
   programs.kitty.settings = {
-    # https://fsd.it/shop/fonts/pragmatapro/
     font_family = "MonoLisa";
-    font_size = "16.0";
+    font_size = "17.0";
     adjust_line_height = "120%";
-    disable_ligatures = "cursor"; # disable ligatures when cursor is on them
-    # Window layout
-    hide_window_decorations = "titlebar-only";
+    disable_ligatures = "cursor";
+    hide_window_decorations = "yes";
+    scrollback_lines = "50000";
 
-    scrollback_lines = "10000";
-    # Tab bar
-    # tab_bar_style = "powerline";
-    # tab_title_template = "{index}: {title}";
-    # active_tab_font_style = "bold";
-    # inactive_tab_font_style = "normal";
-    # tab_activity_symbol = "";
+    # The interval (in seconds) at which to blink the cursor. Set to zero to
+    # disable blinking.
+    cursor_blink_interval = "0.5";
+    # Stop blinking cursor after the specified number of seconds of keyboard inactivity. Set to
+    # zero or a negative number to never stop blinking.
+    cursor_stop_blinking_after = "10.0";
+    # The width (in pts) of window borders. Will be rounded to the nearest number of pixels based on screen resolution.
+    window_border_width = "0.7pt";
+    draw_minimal_borders = "yes";
+
+    listen_on = "unix:/tmp/mykitty";
+
+    # normal
+    color0 = "#20232A";
+    color1 = "#e86671";
+    color2 = "#98c379";
+    color3 = "#e0af68";
+    color4 = "#61afef";
+    color5 = "#c678dd";
+    color6 = "#56b6c2";
+    color7 = "#798294";
+
+    # bright";
+    color8 = "#5c6370";
+    color9 = "#e86671";
+    color10 = "#98c379";
+    color11 = "#e0af68";
+    color12 = "#61afef";
+    color13 = "#c678dd";
+    color14 = "#56b6c2";
+    color15 = "#abb2bf";
+
+    # extended colors";
+    color16 = "#d19a66";
+    color17 = "#f65866";
+
+    url_color = "#98c379";
+
+    background = "#282c34";
+    foreground = "#abb2bf";
+    selection_background = "#393f4a";
+    selection_foreground = "#abb2bf";
+
+    # Tabs
+    active_tab_background = "#282c34";
+    active_tab_foreground = "#abb2bf";
+    inactive_tab_background = "#242b38";
+    inactive_tab_foreground = "#5c6370";
+
   };
 
-  # Change the style of italic font variants
-  # programs.kitty.extraConfig = ''
-  #   font_features PragmataProMonoLiga-Italic +ss06
-  #   font_features PragmataProMonoLiga-BoldItalic +ss07
-  # '';
-  # # }}}
-
-  # Colors config ------------------------------------------------------------------------------ {{{
-  programs.kitty.extras.colors = with pkgs.lib.colors; {
-    enable = true;
-
-    # Colors that aren't dependent on background
-    common = with pkgs.lib.colors.solarized.colors; {
-      # black
-      color0 = "#${darkBasehl}";
-      color8 = "#${darkBase}";
-      # red
-      color1 = "#${red}";
-      color9 = "#${orange}";
-      # green
-      color2 = "#${green}";
-      color10 = "#${darkestTone}";
-      # yellow
-      color3 = "#${yellow}";
-      color11 = "#${darkTone}";
-      # blue
-      color4 = "#${blue}";
-      color12 = "#${lightTone}";
-      # magenta
-      color5 = "#${magenta}";
-      color13 = "#${violet}";
-      # cyan
-      color6 = "#${cyan}";
-      color14 = "#${lightestTone}";
-      # white
-      color7 = "#${lightBasehl}";
-      color15 = "#${lightBase}";
-      # url underline color to fit colors
-      url_color = "#${blue}";
-      # tab bar
-      active_tab_foreground = "#${lightBase}";
-      active_tab_background = "#${green}";
-      inactive_tab_foreground = "#${lightBase}";
-    };
-
-    # Background dependent colors
-    dark = backgroundDependantColors solarized.dark;
-    light = backgroundDependantColors solarized.light;
+  programs.kitty.keybindings = {
+    "kitty_mod+t" = "new_tab_with_cwd";
+    "kitty_mod+enter" = "new_window_with_cwd";
+    "cmd+t" = "new_tab_with_cwd";
+    "cmd+enter" = "new_window_with_cwd";
+    "cmd+k" =
+      "combine : clear_terminal scrollback active : send_text normal x0c";
+    "ctrl+f" =
+      "launch --type=overlay --stdin-source=@screen_scrollback fzf --no-sort --no-mouse --exact -i";
+    "cmd+f" =
+      "launch --type=overlay --stdin-source=@screen_scrollback fzf --no-sort --no-mouse --exact -i";
   };
-
-  # }}}
 }
-# vim: foldmethod=marker
