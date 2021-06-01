4c9111c25b68a0ac43aae103667ec01c1222f74b
Pritam Kadam <kpritam@thoughtworks.com>
2021-06-01 16:42:21 +0530

    Remove unused files and update readme

--- install.sh
+++ install.sh
@@ -8,6 +8,17 @@ nix build .#darwinConfigurations.bootstrap.system
 
 darwin-rebuild switch --flake .#MacBookPro
 
+echo "Upgrading stackline ..."
+cd ~/.hammerspoon/stackline && git pull
+
+echo "Upgrading simple-bar ..."
+cd "~/Library/Application Support/†bersicht/widgets/simple-bar" && git pull
+
+echo "Upgrading neovim ..."
 brew reinstall neovim
+
+echo "Upgrading npm packages ..."
 npm update -y
+
+echo "Upgrading python packages ..."
 pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U
