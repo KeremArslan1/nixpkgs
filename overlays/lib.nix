35ce17b327f5794ba4cb22ee47118be2b5ec6ec0
Pritam Kadam <kpritam@thoughtworks.com>
2021-06-01 17:17:46 +0530

    Revert lib overlays

---
+++ overlays/lib.nix
@@ -0,0 +1,10 @@
+final: prev: {
+  lib = prev.lib // rec {
+    # Other
+    OS = if prev.stdenv.isDarwin then
+      "macOS"
+    else
+      builtins.elemAt (builtins.match ''NAME="?([A-z]+)"?.*''
+        (builtins.readFile /etc/os-release)) 0;
+  };
+}
